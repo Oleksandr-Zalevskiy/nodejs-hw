7d75fe291c54408d8edb620da6c4f00468b23e30
zalevskiy <zaleva100@gmail.com>
2026-03-14 20:29:56 +0200

    fix config files and eslint dependencies

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import pinoHttp from "pino-http";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pinoHttp from 'pino-http';
 
 dotenv.config();
 
@@ -11,15 +11,21 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(pinoHttp());
+app.use(
+  pinoHttp({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
 
-app.get("/notes", (req, res) => {
+app.get('/notes', (req, res) => {
   res.status(200).json({
-    message: "Retrieved all notes",
+    message: 'Retrieved all notes',
   });
 });
 
-app.get("/notes/:noteId", (req, res) => {
+app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
 
   res.status(200).json({
@@ -27,13 +33,13 @@ app.get("/notes/:noteId", (req, res) => {
   });
 });
 
-app.get("/test-error", (req, res) => {
-  throw new Error("Simulated server error");
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
 });
 
 app.use((req, res) => {
   res.status(404).json({
-    message: "Route not found",
+    message: 'Route not found',
   });
 });
 

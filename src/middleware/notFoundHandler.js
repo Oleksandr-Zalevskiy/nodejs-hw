7d543dfea91b7bfa5f461c5e513550c255cad2e6
zalevskiy <zaleva100@gmail.com>
2026-03-26 18:41:45 +0200

    fix mentor review comments

--- src/middleware/notFoundHandler.js
+++ src/middleware/notFoundHandler.js
@@ -1,5 +1,13 @@
-export const notFoundHandler = (req, res) => {
-  res.status(404).json({
-    message: 'Route not found',
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    message: 'Something went wrong',
   });
 };

8086381411319ac440107cc532e278fd9e8bf57d
zalevskiy <zaleva100@gmail.com>
2026-04-06 22:15:38 +0300

    fix: add noteValidation

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,45 +1,17 @@
 import Joi from 'joi';
-import { isValidObjectId } from 'mongoose';
-import { TAGS } from '../constants/tags.js';
 
-const objectIdField = Joi.string().custom((value, helpers) => {
-  if (!isValidObjectId(value)) {
-    return helpers.error('any.invalid');
-  }
+export const createNoteSchema = Joi.object({
+  title: Joi.string().required(),
+  content: Joi.string().allow(''),
+  tag: Joi.string(),
+});
 
-  return value;
-}, 'ObjectId validation');
+export const updateNoteSchema = Joi.object({
+  title: Joi.string(),
+  content: Joi.string().allow(''),
+  tag: Joi.string(),
+});
 
-export const getAllNotesSchema = {
-  query: Joi.object({
-    page: Joi.number().integer().min(1).default(1),
-    perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...TAGS),
-    search: Joi.string().allow(''),
-  }),
-};
-
-export const noteIdSchema = {
-  params: Joi.object({
-    noteId: objectIdField.required(),
-  }),
-};
-
-export const createNoteSchema = {
-  body: Joi.object({
-    title: Joi.string().min(1).required(),
-    content: Joi.string().allow(''),
-    tag: Joi.string().valid(...TAGS),
-  }),
-};
-
-export const updateNoteSchema = {
-  params: Joi.object({
-    noteId: objectIdField.required(),
-  }),
-  body: Joi.object({
-    title: Joi.string().min(1),
-    content: Joi.string().allow(''),
-    tag: Joi.string().valid(...TAGS),
-  }).min(1),
-};
+export const noteIdSchema = Joi.object({
+  noteId: Joi.string().hex().length(24).required(),
+});

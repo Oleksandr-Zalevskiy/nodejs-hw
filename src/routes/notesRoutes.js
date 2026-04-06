a36aab27735a906dc05148d49527981e9626a82d
zalevskiy <zaleva100@gmail.com>
2026-04-06 20:43:51 +0300

    apdate routes

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -6,7 +6,6 @@ import {
   updateNote,
   deleteNote,
 } from '../controllers/notesController.js';
-import authenticate from '../middleware/authenticate.js';
 import isValidId from '../middlewares/isValidId.js';
 
 const notesRouter = Router();

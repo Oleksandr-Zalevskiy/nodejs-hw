5489ab704998217323dcd85b69b86184fd577c9b
zalevskiy <zaleva100@gmail.com>
2026-04-06 21:00:30 +0300

    fix errorHandler and notFoundHandler imports

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ import { errors } from 'celebrate';
 
 import notesRouter from './routes/notesRoutes.js';
 import connectMongoDB from './db/connectMongoDB.js';
-import { logger } from './middleware/logger.js';
-import { notFoundHandler } from './middleware/notFoundHandler.js';
-import { errorHandler } from './middleware/errorHandler.js';
+import logger from './middleware/logger.js';
+import notFoundHandler from './middleware/notFoundHandler.js';
+import errorHandler from './middleware/errorHandler.js';
 
 dotenv.config();
 

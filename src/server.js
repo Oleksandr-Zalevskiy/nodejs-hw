d1d0588ff32b5ab3d0dd690f6f4bbbc548666d26
zalevskiy <zaleva100@gmail.com>
2026-04-06 20:56:05 +0300

    fix connectMongoDB import

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import { errors } from 'celebrate';
 
 import notesRouter from './routes/notesRoutes.js';
-import { connectMongoDB } from './db/connectMongoDB.js';
+import connectMongoDB from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';

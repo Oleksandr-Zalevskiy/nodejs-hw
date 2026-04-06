88293d439e99c596a2bf44431ee29d404c2fce79
zalevskiy <zaleva100@gmail.com>
2026-04-06 23:10:12 +0300

    feat: add missing controller functions

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,6 +1,7 @@
+import { Note } from '../models/note.js';
+
 export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search } = req.query;
-
   const filter = { userId: req.user._id };
 
   if (tag) filter.tag = tag;
@@ -9,7 +10,7 @@ export const getAllNotes = async (req, res) => {
   const skip = (page - 1) * perPage;
 
   const [notes, total] = await Promise.all([
-    Note.find(filter).skip(skip).limit(perPage),
+    Note.find(filter).skip(skip).limit(Number(perPage)),
     Note.countDocuments(filter),
   ]);
 
@@ -20,4 +21,54 @@ export const getAllNotes = async (req, res) => {
     totalPages: Math.ceil(total / perPage),
     notes,
   });
+};
+
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOne({ _id: noteId, userId: req.user._id });
+
+  if (!note) {
+    return res.status(404).json({ message: 'Note not found' });
+  }
+
+  res.json(note);
+};
+
+export const createNote = async (req, res) => {
+  const note = await Note.create({
+    ...req.body,
+    userId: req.user._id,
+  });
+  res.status(201).json(note);
+};
+
+// 4. Оновлення нотатки
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndUpdate(
+    { _id: noteId, userId: req.user._id },
+    req.body,
+    { new: true },
+  );
+
+  if (!note) {
+    return res.status(404).json({ message: 'Note not found' });
+  }
+
+  res.json(note);
+};
+
+// 5. Видалення нотатки
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+    userId: req.user._id,
+  });
+
+  if (!note) {
+    return res.status(404).json({ message: 'Note not found' });
+  }
+
+  res.status(204).send();
 };

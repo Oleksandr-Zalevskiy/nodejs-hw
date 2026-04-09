eb3023f3f52ada43f4e772498fedb9d742049d5b
zalevskiy <zaleva100@gmail.com>
2026-04-09 20:20:59 +0300

    fix: named exports for middlewares

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,10 +1,7 @@
-const errorHandler = (err, req, res, next) => {
+export const errorHandler = (err, req, res, next) => {
   const status = err.status || 500;
-  const message = err.message || 'Something went wrong';
 
   res.status(status).json({
-    message,
+    message: err.message || 'Internal Server Error',
   });
 };
-
-export default errorHandler;

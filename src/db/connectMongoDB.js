eeac7fae36566222a357098a9449cdbcb0a79e5b
zalevskiy <zaleva100@gmail.com>
2026-04-09 21:11:54 +0300

    fix: correct path for env.js without utils folder

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -8,17 +8,13 @@ export const connectMongoDB = async () => {
     const url = env('MONGODB_URL');
     const db = env('MONGODB_DB');
 
-    // Формуємо рядок підключення.
-    // Переконайтеся, що в MongoDB Atlas дозволено доступ з усіх IP (0.0.0.0/0)
     const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
 
     await mongoose.connect(connectionString);
 
     console.log('Mongo connection successfully established!');
   } catch (e) {
-    console.error('Error while setting up mongo connection', e.message);
-    // Важливо: якщо база не підключилася, ми зупиняємо процес,
-    // щоб Render міг перезапустити додаток
+    console.error('Error while setting up mongo connection:', e.message);
     process.exit(1);
   }
 };

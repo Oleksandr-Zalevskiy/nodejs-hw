8edcd1779a7eb2661a68f234a30bc75f1e005565
zalevskiy <zaleva100@gmail.com>
2026-04-11 10:43:56 +0300

    fix: final check for env paths and variables

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,24 +1,20 @@
 import mongoose from 'mongoose';
+import { env } from '../env.js'; // Дві крапки - виходимо з db у src
 
 export const connectMongoDB = async () => {
   try {
-    const user = process.env.MONGODB_USER;
-    const password = process.env.MONGODB_PASSWORD;
-    const url = process.env.MONGODB_URL;
-    const db = process.env.MONGODB_DB;
+    // Ці назви мають бути ТАКИМИ Ж, як Key на Render
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
 
-    // перевірка env (щоб одразу бачити помилку)
-    if (!user || !password || !url || !db) {
-      throw new Error('MongoDB env variables not set');
-    }
-
-    const connectionString = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority`;
+    const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
 
     await mongoose.connect(connectionString);
-
-    console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('MongoDB error:', error.message);
+    console.log('Mongo connection successfully established!');
+  } catch (e) {
+    console.error('MongoDB connection error:', e.message);
     process.exit(1);
   }
 };

920e16f4848b9692152a1b720e1b5951c5415ed1
zalevskiy <zaleva100@gmail.com>
2026-04-12 20:58:11 +0300

    apdate import User from '../models/user.js';

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
-import { User } from '../models/user.js';
+import User from '../models/user.js';
 import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 

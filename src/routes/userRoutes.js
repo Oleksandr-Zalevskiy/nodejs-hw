bc0933a638b75a0d4617e70eb3802321d2c646ee
zalevskiy <zaleva100@gmail.com>
2026-04-16 21:40:19 +0300

    fix authenticate import

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import authenticate from '../middleware/authenticate.js';
+import { authenticate } from '../middleware/authenticate.js';
 import { upload } from '../middleware/multer.js';
 import { updateUserAvatar } from '../controllers/userController.js';
 

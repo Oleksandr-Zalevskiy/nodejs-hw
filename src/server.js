b7f5388f9313bf00d7eab9426f55df694dfc642f
zalevskiy <zaleva100@gmail.com>
2026-04-17 15:45:38 +0300

    add apdate server

--- src/server.js
+++ src/server.js
@@ -5,22 +5,25 @@ import 'dotenv/config';
 
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import notesRoutes from './routes/notesRoutes.js'; // Має бути тут
 
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js'; // Має бути тут
 
 import { errors } from 'celebrate';
-
 import { connectMongoDB } from './db/connectMongoDB.js';
 
 const app = express();
 
+app.use(logger); // Має бути тут
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(userRoutes);
+app.use(notesRoutes); // Має бути тут
 
 app.use(notFoundHandler);
 app.use(errors());
